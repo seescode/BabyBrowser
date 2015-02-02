bec0c2309af1d844a86faa03f62747bb35b6be50
seescode <seesyong@gmail.com>
2015-02-02 09:07:00 -0500

    fixed bug, got the thing working

--- dist/js/game.js
+++ dist/js/game.js
@@ -99,18 +99,20 @@ module.exports = Menu;
   function Play() {}
   Play.prototype = {
     create: function() {
-    //  A simple background for our game
-    this.add.sprite(0, 0, 'sky');
+		var me = this;
+		
+		//  A simple background for our game
+		this.add.sprite(0, 0, 'sky');
 	  
 		this.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
 		
 		this.input.keyboard.onDownCallback = function(e) {
 			//TODO instead of outputting A have it output random japanese characters
-			this.add.text(this.rnd.integerInRange(0, 750), this.rnd.integerInRange(0, 550), 'A', { fontSize: '32px', fill: '#000' });	
+			me.add.text(me.rnd.integerInRange(0, 750), me.rnd.integerInRange(0, 550), 'A', { fontSize: '32px', fill: '#000' });	
 			
-			if (!this.scale.isFullScreen)
+			if (!me.scale.isFullScreen)
 			{
-				this.scale.startFullScreen(true);	
+				me.scale.startFullScreen(true);	
 			}
 		}		
 	  
