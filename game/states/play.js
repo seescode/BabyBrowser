e7b64165f2fd840682f2265a3af1156ce1975f99
seescode <seesyong@gmail.com>
2015-04-13 14:15:49 +0900

    Get random character instead of just always 'A'

--- game/states/play.js
+++ game/states/play.js
@@ -4,22 +4,34 @@
   Play.prototype = {
     create: function() {
 		var me = this;
-		
+
+    var getRandomCharacter = function() {
+      var array = ['A', 'B', 'C', 'D', 'E']
+      var index = me.rnd.integerInRange(0, 4);
+
+      return array[index];
+    };
+
 		//  A simple background for our game
 		this.add.sprite(0, 0, 'sky');
-	  
+
 		this.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
-		
+
 		this.input.keyboard.onDownCallback = function(e) {
 			//TODO instead of outputting A have it output random japanese characters
-			me.add.text(me.rnd.integerInRange(0, 750), me.rnd.integerInRange(0, 550), 'A', { fontSize: '32px', fill: '#000' });	
-			
+			me.add.text(
+        me.rnd.integerInRange(0, 750),
+        me.rnd.integerInRange(0, 550),
+        getRandomCharacter(),
+        { fontSize: '32px', fill: '#000' }
+      );
+
 			if (!me.scale.isFullScreen)
 			{
-				me.scale.startFullScreen(true);	
+				me.scale.startFullScreen(true);
 			}
-		}		
-	  
+		}
+
     },
     update: function() {
 
@@ -28,5 +40,7 @@
       //this.game.state.start('gameover');
     }
   };
-  
+
+
+
   module.exports = Play;
